d56bea3c9b077fa5487da4d4d5c4cea34924decc
MichaelWiss <michael.wiss@gmail.com>
2016-12-08 11:38:52 -0600

    simple

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 // create a new component
 
 const App = function() {
 	return <div>Hi!</div>;
 }
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
