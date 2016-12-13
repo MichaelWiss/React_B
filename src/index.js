58d9c64ee0ed1ac58a3a563d164f4bd6ed30441c
MichaelWiss <michael.wiss@gmail.com>
2016-12-13 13:12:40 -0600

    search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Config from './components/config';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
 
 
 const API_KEY = Config.API_KEY;
+
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+})
 // create a new component
+
+
 
 const App = () => {
 	return (
