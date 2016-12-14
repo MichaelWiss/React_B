10b9b41f038c1befaeb32d8ea1b33e0ba28cbd9e
MichaelWiss <michael.wiss@gmail.com>
2016-12-14 17:56:10 -0600

    template

--- src/components/videolistitem.js
+++ src/components/videolistitem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-	return <ul>Video</ul>
+const VideoListItem = ({video}) =>  {
+    return (
+    	<ul className="list-group-item">
+    	  <div className="video-list media">
+            <div className="media-left">
+              <img className="media-object" />
+            </div>
+            <div className="media-body">
+             <div className="media-heading"></div>
+            </div>
+           </div>
+    	</ul>
+    );
 };
 
 export default VideoListItem;
