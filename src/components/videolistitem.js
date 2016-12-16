4d90aa391a59a5c30e97c7d94e69e27754ee09cb
MichaelWiss <michael.wiss@gmail.com>
2016-12-16 11:47:23 -0600

    props to onClick event

--- src/components/videolistitem.js
+++ src/components/videolistitem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) =>  {
+const VideoListItem = ({video, onVideoSelect}) =>  {
   const imageUrl = video.snippet.thumbnails.default.url;
 
 
 
     return (
-    	<ul className="list-group-item">
+    	<ul onClick={() => onVideoSelect(video)} className="list-group-item">
     	  <div className="video-list media">
             <div className="media-left">
               <img className="media-object" src={imageUrl} />
